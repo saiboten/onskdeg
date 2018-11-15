23982d438453cb6a5b5f5e7b8a971f33a36c18e8
saiboten <saiboten@gmail.com>
2018-11-15 21:11:38 +0100

    Checking in built files, yup

---
+++ build/precache-manifest.c41763fc9dcdb9ce94a348b43ec6ed1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ae13bc863b5aebd90257119aa4b22f0",
+    "url": "/static/media/facebook-icon.3ae13bc8.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9c834b738dd9c1f2f22e",
+    "url": "/static/js/main.9c834b73.chunk.js"
+  },
+  {
+    "revision": "8e4aa46ca0af75f8226f",
+    "url": "/static/js/1.8e4aa46c.chunk.js"
+  },
+  {
+    "revision": "9c834b738dd9c1f2f22e",
+    "url": "/static/css/main.ffc89761.chunk.css"
+  },
+  {
+    "revision": "2b794c6ab34bf70e96fe8289dc0ef441",
+    "url": "/index.html"
+  }
+];
