519b03dedab4c7b031b50619c347097879be6c57
saiboten <saiboten@gmail.com>
2018-11-19 22:27:26 +0100

    Moved circle stuff into its own component pluss animated them a bit

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -4,52 +4,19 @@ import styled from 'styled-components';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 
 import firebase from '../firebase/firebase';
-import facebook from '../firebase/facebooklogin';
 import { User } from '../../types/types';
 import ThirdPartyLogin from './ThirdPartyLogin';
 import InternalLogin from './InternalLogin';
-import colors from '../../styles/colors';
+import FloatingCircles from '../common/FloatingCircles';
 
 const debug = require('debug')('SelectUser');
 
-require('./selectuser.scss');
-
 const StyledParagraph = styled.p`
-  font-size: 3rem;
-  margin: 2rem;
+font-size: 3rem;
+margin: 2rem;
 `;
 
-const StyledCircleContainer = styled.div`
-  position: relative;
-  width: 100%;
-  height: 100%;
-  overflow: hidden;
-`;
-
-function getRandomInt(min: number, max: number) {
-  var num = Math.floor((Math.random() * Math.floor(max)));
-  if(num < min) {
-    num += min;
-  }
-  return num;
-}
-
-interface StyledCircleProps {
-  size: string;
-  top: string;
-  left: string;
-}
-
-const StyledCircle = styled.div`
-  background-color: ${colors.goldDark};
-  border-radius: 50%;
-  filter: opacity(.5);
-  position: absolute;
-  width: ${(props: StyledCircleProps) => props.size};
-  height: ${props => props.size};
-  top: ${props => props.top};
-  left: ${props => props.left};
-`;
+require('./selectuser.scss');
 
 const LoginContainer = styled.div`
   display: flex;
@@ -105,13 +72,7 @@ class Login extends React.Component<Props, State> {
       font-weight: 400;
     `;
 
-    const circles = Array(15).fill(0).map(
-      (el, index) =>
-        <StyledCircle
-          key={index}
-          size={`${getRandomInt(50, 500)}px`}
-          top={`${getRandomInt(0, 100)}%`}
-          left={`${getRandomInt(0, 100)}%`} />);
+    
 
     return (
       <BrowserRouter>
@@ -122,9 +83,7 @@ class Login extends React.Component<Props, State> {
             <Route component={ThirdPartyLogin} />
           </Switch>
           <StyledParagraph>{feedback}</StyledParagraph>
-          <StyledCircleContainer>
-            {circles}
-          </StyledCircleContainer>
+          <FloatingCircles />
         </LoginContainer>
       </BrowserRouter>
     );
