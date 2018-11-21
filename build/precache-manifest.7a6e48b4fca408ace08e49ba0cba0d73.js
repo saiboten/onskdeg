8cdba173cc0e63d0fe59cbccfb20f2b2d940af8e
saiboten <saiboten@gmail.com>
2018-11-21 23:09:31 +0100

    New build

---
+++ build/precache-manifest.7a6e48b4fca408ace08e49ba0cba0d73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ae13bc863b5aebd90257119aa4b22f0",
+    "url": "/static/media/facebook-icon.3ae13bc8.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "83dd565f73946a8cdea4",
+    "url": "/static/js/main.83dd565f.chunk.js"
+  },
+  {
+    "revision": "d253fc6b509ca3aadcc5",
+    "url": "/static/js/1.d253fc6b.chunk.js"
+  },
+  {
+    "revision": "83dd565f73946a8cdea4",
+    "url": "/static/css/main.a3e50c78.chunk.css"
+  },
+  {
+    "revision": "3ebc4ddd933d9ec507a04b5a3acadf2c",
+    "url": "/index.html"
+  }
+];
