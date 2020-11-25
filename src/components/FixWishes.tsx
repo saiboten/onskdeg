13ea4867ef46564344b40b550e7352d5babeb1f5
saiboten <saiboten@gmail.com>
2020-11-25 21:19:35 +0100

    script to add date

--- src/components/FixWishes.tsx
+++ src/components/FixWishes.tsx
@@ -2,26 +2,28 @@ import React from "react";
 import { Wish } from "../types/types";
 import { Container } from "./common/Container";
 import firebase from "./firebase/firebase";
+import { parse } from "date-fns";
+import { isUndefined } from "util";
 
 export const FixWishes = () => {
   async function go() {
-    const allWishes = await firebase.firestore().collection("wishes").get();
-    allWishes.docs.forEach((m) => {
-      const wishList: Wish[] = [...(m.data()?.wishes || [])];
+    const allWishes = await firebase.firestore().collection("wish").get();
+    allWishes.docs.forEach(async (m) => {
+      const ref = firebase.firestore().collection("wish").doc(m.id);
 
-      wishList.forEach(async (wish) => {
-        const newWish: Wish = {
-          ...wish,
-          owner: m.id,
-        };
+      const data = await ref.get();
 
-        delete newWish.wishes;
-
-        const ref = await firebase.firestore().collection("wish").add(newWish);
-        await ref.update({
-          id: ref.id,
+      if (data.data()?.date !== undefined) {
+        console.log("date not null. Doing nothing");
+        return;
+      } else {
+        console.log("date undefined - setting date");
+        ref.update({
+          date: firebase.firestore.Timestamp.fromDate(
+            parse("2020-11-01", "yyyy-MM-dd", new Date())
+          ),
         });
-      });
+      }
     });
   }
 
